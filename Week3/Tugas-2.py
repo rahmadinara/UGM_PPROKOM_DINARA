50507402d00e8309963646c419dd1b50b7932bb3
Rahma Dinara Safia Ardianti <rahmadinarasafiaardianti@mail.ugm.ac.id>
2024-08-28 19:37:22 +0700

    Add files via upload

---
+++ Week3/Tugas-2.py
@@ -0,0 +1,9 @@
+m = ['Alice', 'ants', 'Bob', 'badgers', 'Carol', 'cats']
+m.sort()
+m
+print(m)
+
+spam = ['a', 'z', 'A', 'Z']
+spam.sort(key=str.lower)
+spam
+print(spam)
---
+++ Week3/Tugas-5.py
@@ -0,0 +1,21 @@
+#penggabungan string dengan simbol "+"
+blog = 'Klinik' + 'Python'
+print(blog)
+
+# memperbanyak string dengan simbol "*"
+newblog = blog*5
+print(newblog)
+
+# memperbanyak string dengan simbol ""
+blog *= 4
+print(blog)
+
+string = 'Klinik' 'Python'
+print(string)
+print("\n")
+
+buah = 'Nanas'
+print(buah[0])
+print(buah[0:2])
+print(buah[0:4])
+print(buah[0:5])
